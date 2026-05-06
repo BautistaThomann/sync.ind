a3f06f832b0cd523659d715fb85db305311cd047
BautistaThomann <thomannbautista@gmail.com>
2026-05-06 12:34:54 -0300

    fin web vista pc

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const track = document.getElementById("track");
+
+    for (let i = 0; i < 20; i++) {
+        const p = document.createElement("p");
+        p.textContent = "SYNC.IND - NEW IN";
+        track.appendChild(p);
+    }
